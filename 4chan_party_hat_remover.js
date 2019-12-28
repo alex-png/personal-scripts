07af6895b337e3ac076478eccb1e8f96e247dcbd
alex-png <alexrod102797@gmail.com>
2019-12-27 23:44:23 -0500

    cleaner

--- 4chan_party_hat_remover.js
+++ 4chan_party_hat_remover.js
@@ -1,7 +1,6 @@
 (function removeHats() {
     'use strict';
 
-    // Your code here...
     let hats = document.getElementsByClassName('party-hat')
     while (hats.length > 0 ){
         for (const h of hats) {
